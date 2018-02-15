7d40ba686af9aed545b21ad4b78be650ae31964b
Btara Truhandarien <btara.truhandarien@gmail.com>
2018-02-15 09:53:01 +0900

    Add installation details

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # What is this
 
 Btruhand's settings for work and personal things
+
+# Installations
+Please see installations.sh and run
+```
+./installations.sh
+```
+
+## Other installations
+- Chrome: https://www.google.com/chrome/browser/desktop/index.html
+- Vagrant: https://www.vagrantup.com/downloads.html
+- Virtual Box: https://www.virtualbox.org/wiki/Downloads
+- Windows in Virtual Box: http://sites.miis.edu/kb/2012/06/19/how-to-install-virtualbox-and-windows-on-your-mac/
+- iTerm2: https://www.iterm2.com/downloads.html
+  - Make sure to setup iterm to use saved settings in ```iterm2``` folder
+- Docker: https://docs.docker.com/docker-for-mac/install/
---
+++ installations.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+mkdir -p ~/Code/
+git clone https://github.com/btruhand/settings.git ~/Code/
+
+# prepare npm-packages folder
+mkdir ~/.npm-packages
+# initialize bash profile
+cp ~/Code/settings/dotfiles/bash_profile .bash_profile
+. .bash_profile
+
+# Copy bashrc
+dotcp bashrc
+
+# Copy all remaining dotfiles
+dotcp tern-project
+dotcp vimrc
+dotcp eslintrc.json
+dotcp gitconfig
+
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install node
+brew install node
+## Install n
+npm install -g n
+## Install latest lts node version
+n lts
+
+# Install jsctags
+npm install -g jsctags
+# Install python3
+brew install python3
+# Install vim
+brew install vim --with-override-system-vi
+## Install stereokai color scheme
+mkdir ~/.vim/colors
+git clone https://github.com/gummesson/stereokai.vim.git ~/.vim/stereokai
+cp -r ~/.vim/stereokai/colors/stereokai.vim ~/.vim/colors/
+rm -rf ~/.vim/stereokai
+## Install vim plugins
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall
+
+# Setting up YCM
+## Install cmake
+brew install cmake
+## Install YCM
+~/.vim/bundle/YouCompleteMe/./install.py --tern-completer
+
+
+# Setting up tern_for_vim
+cd ~/.vim/bundle/tern_for_vim/ && npm install
+# Install rbenv
+brew install rbenv
+## Install ruby v2.3.0
+rbenv install 2.3.0
+
+# install bundler and ripper-tags
+rbenv local 2.3.0
+gem install bundler
+gem install ripper-tags
+# install latest git
+brew install git
+
+# install php
+brew tap homebrew/homebrew-php
+
+
+# base
+brew install php56
+
+
+#xedebug
+brew install php56-xdebug
+brew install php56-intl
+brew install php56-mcrypt
+## install phpctags
+curl -Ss http://vim-php.com/phpctags/install/phpctags.phar > ~/Code/tools/phpctags
+
+# install java
+brew cask install java
+
+# install graphviz (mainly for plantuml)
+## Install libtool
+brew install libtool
+## Install graphviz
+brew install graphviz
+
+# brew health checkup
+brew doctor
+
+# make public key
+ssh-keygen -t rsa
+cat ~/.ssh/id_rsa.pub

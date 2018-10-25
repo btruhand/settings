e0eeb475e30b4db37b413d60f2b22ebfe4379bff
Btara Truhandarien <btara.truhandarien@gmail.com>
2018-10-25 16:33:24 -0700

    Added ability to set target home for provisioning and bootstrapping

--- installations.sh
+++ installations.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 INSTALL=$1
+# assume target home is given as second argument for now
+TARGET_HOME="$2"
 
 VALIDINSTALLSTEPS=(
   'provision' 'build-brew' 'build-git'
@@ -19,7 +21,7 @@ is_single_install_valid() {
 print_usage() {
   echo "./installations.sh [script-step]"
   echo "Arguments:"
-  echo "   script-step    bootstrap, provision, build-brew"
+  echo "   script-step    provision, bootstrap, build-brew"
   echo "                  build-git, build-node, build-ruby, build-packagetools"
   echo "                  build-vim, build-python, build-go"
   echo "                  security"
@@ -39,5 +41,5 @@ fi
 
 source ./installers/scripts/routines.sh
 for STEP in "${INSTALL[@]}"; do
-  source ./installers/scripts/$STEP $(pwd)
+  source ./installers/scripts/$STEP $(pwd) "$2"
 done

cb7ea2b44fc9131ddd224dfaf31b255efdb5b6f4
Btara Truhandarien <10100624+btruhand@users.noreply.github.com>
2021-08-17 21:23:23 -0400

    refactor: remove extras directory

--- dotfiles/extras/sfu.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/local/bin/bash
-
-export HADOOP_HOME="/Users/btaratruhandarien/Documents/SFU_studies/courses/CMPT732/hadoop-3.1.1"
-export JAVA_HOME="$(/usr/libexec/java_home)"
-export PYSPARK_PYTHON=python3
